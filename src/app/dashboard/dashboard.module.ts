ede2fa61c73f8b06803a1370cbbaba33979591ae
Ievgen <baranoffei@gmail.com>
2021-04-14 22:26:34 +0300

    #card-59062053 (#2)
    Add angular-material, git action and base nav

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardContainer } from './containers/dashboard-container/dashboard-container.component';
+
+const routes: Routes = [{ path: '', component: DashboardContainer }];
 
 @NgModule({
   imports: [
     CommonModule,
-    DashboardRoutingModule
-  ]
+    MatTabsModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [
+    DashboardContainer
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class DashboardModule { }
