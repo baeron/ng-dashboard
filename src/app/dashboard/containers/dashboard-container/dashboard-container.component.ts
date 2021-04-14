22c361331896783f2ea81b1d08bfc2f2862a6386
Ievgen <baranoffei@gmail.com>
2021-04-14 19:59:41 +0300

    Create base project structure

---
+++ src/app/dashboard/containers/dashboard-container/dashboard-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-container',
+  templateUrl: './dashboard-container.component.html',
+  styleUrls: ['./dashboard-container.component.scss']
+})
+export class DashboardContainer implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
